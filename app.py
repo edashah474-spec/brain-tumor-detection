5a5c844f98eead941ca4a85867cc30aa50442bd1
edashah474-spec <edashah474@gmail.com>
2026-04-06 12:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
+
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 from PIL import Image
 import gdown
@@ -16,18 +16,15 @@ st.set_page_config(
 def load_model():
     model_path = "best_model.keras"
     if not os.path.exists(model_path):
-        with st.spinner("Loading AI model... please wait 30 seconds"):
+        with st.spinner("Downloading model... please wait"):
             file_id = "1_xnYZcf3qNoLUG_AUve0SaqzexdZxNwZ"
             url = f"https://drive.google.com/uc?id={file_id}"
             gdown.download(url, model_path, quiet=False)
+    import tensorflow as tf
     model = tf.keras.models.load_model(model_path)
     return model
 
-model = load_model()
-CLASSES = ["Glioma", "Meningioma", "No Tumor", "Pituitary"]
-
 st.title("🧠 Brain Tumor Detection from MRI")
-st.write("Upload a brain MRI scan — AI will detect the tumor type instantly")
 st.write("**Accuracy: 97.68% | EfficientNetV2 | Built by Noor Khan**")
 st.divider()
 
@@ -37,13 +34,15 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
+    CLASSES = ["Glioma", "Meningioma", "No Tumor", "Pituitary"]
+    
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded MRI Scan", width=300)
 
-    img_array = np.array(image.resize((224, 224))).astype(np.float32)
-    img_array = np.expand_dims(img_array, axis=0)
-
-    with st.spinner("Analyzing MRI scan..."):
+    with st.spinner("Loading AI model and analyzing... (first load takes 1-2 mins)"):
+        model = load_model()
+        img_array = np.array(image.resize((224, 224))).astype(np.float32)
+        img_array = np.expand_dims(img_array, axis=0)
         preds = model.predict(img_array, verbose=0)[0]
         pred_class = CLASSES[np.argmax(preds)]
         confidence = np.max(preds) * 100
@@ -60,8 +59,8 @@ if uploaded_file is not None:
         st.progress(float(prob), text=f"{cls}: {prob*100:.1f}%")
 
     st.divider()
-    st.warning("⚕️ For educational purposes only. Always consult a qualified radiologist for medical diagnosis.")
+    st.warning("⚕️ For educational purposes only. Always consult a radiologist.")
 
 st.divider()
-st.markdown("**Noor Khan | CS AI-ML | KLE Technological University, Belgaum**")
+st.markdown("**Safa Sayyed | CS AI-ML | KLE Technological University, Belgaum**")
 st.markdown("**GitHub:** github.com/eadashah474-spec/brain-tumor-detection")
